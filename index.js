a61c882eb65f71ec7ce65b56567ba077cb628c7c
krb <dev@blaszczyk.tk>
2020-10-14 08:34:43 +0200

    Fix Default Export

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { requireNativeComponent } from 'react-native';
+
+var PayUWidget = requireNativeComponent('PaymentChooserWidget');
+
+module.exports = PayUWidget;
